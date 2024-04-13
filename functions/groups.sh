c39fbccd67f8077ae3b0ddff46e5624c9b9fb325
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-13 18:25:31 +0100

    m

--- functions/groups.sh
+++ functions/groups.sh
@@ -5,6 +5,6 @@ function make_groups {
     sudo groupadd svxlink
  	sudo useradd -g svxlink -d /etc/svxlink svxlink
  	sudo usermod -aG audio,nogroup,svxlink,plugdev svxlink
-	
+	sudo usermod -aG gpio svxlink
 }
 	
