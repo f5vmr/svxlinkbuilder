cd41a994356cb85008040dcb9d3ae01ed46011e2
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-14 18:35:25 +0100

    m

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 source "${BASH_SOURCE%/*}/functions/sound_card.sh"
 soundcard
+echo $HID $GPIOD $card
+sleep 4
 source "${BASH_SOURCE%/*}/functions/node_type.sh"
 nodeoption
 echo -e "$(date)" "${GREEN} #### Setting up Node #### ${NORMAL}" | tee -a  /var/log/install.log
