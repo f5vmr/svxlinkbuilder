ee65e59d370ceca7cd1c058e556a31969cd10a4d
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-14 19:00:15 +0100

    m

--- test.sh
+++ test.sh
@@ -1,9 +1,25 @@
-source "${BASH_SOURCE%/*}/functions/sound_card.sh"
-soundcard
-echo $HID $GPIOD $card
-sleep 4
-source "${BASH_SOURCE%/*}/functions/node_type.sh"
-nodeoption
-echo -e "$(date)" "${GREEN} #### Setting up Node #### ${NORMAL}" | tee -a  /var/log/install.log
-source "${BASH_SOURCE%/*}/functions/node_setup.sh"
-nodeset
+selected=$(whiptail --title "Metar Info" --checklist "choose which Airports:" 27 78 20 \
+        "EGLL" "London Heathrow" OFF \
+        "EGKK" "London Gatwick" OFF \
+        "EGCC" "Manchester" OFF \
+        "EGBB" "Birmingham" OFF \
+        "EGSS" "London Stansted" OFF \
+        "EGPF" "Glasgow" OFF \
+        "EGPH" "Edinburgh" OFF \
+        "EGPD" "Aberdeen Dyce" OFF \
+        "EGPK" "Prestwick" OFF \
+        "EGHH" "Bournemouth" OFF \
+        "EGHI" "Southampton" OFF \
+        "EGNT" "Newcastle" OFF \
+        "EGNX" "East Midlands" OFF \
+        "EGGW" "London Luton" OFF \
+        "EGGD" "Bristol" OFF \
+        "EGCN" "Sheffield" OFF \
+        "EGNM" "Leeds Bradford" OFF \
+        "EGNS" "Ronaldsway" OFF \
+        "EGAA" "Belfast Aldergrove" OFF \
+        "EGBD" "Belfast City" OFF 3>&1 1>&2 2>&3)
+        selected=$(echo "$selected" | sed 's/"//g')
+        selected=$(echo "$selected" | tr ' ' ',')
+        echo $selected
+        
