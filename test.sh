8054ea9102ced12c035bbfe40a2f20c3b67e4eb0
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-14 18:07:21 +0100

    m

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
-NODE_OPTION="4"
+source "${BASH_SOURCE%/*}/functions/node_type.sh"
+nodeoption
 echo -e "$(date)" "${GREEN} #### Setting up Node #### ${NORMAL}" | tee -a  /var/log/install.log
 source "${BASH_SOURCE%/*}/functions/node_setup.sh"
 nodeset
