fa5c9bc53f68c081551e1ae84206c85bc6e78ce3
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-15 11:37:11 +0100

    m

--- test.sh
+++ test.sh
@@ -1,25 +1,3 @@
-selected=$(whiptail --title "Metar Info" --checklist "choose which Airports:" 27 78 20 \
-        "EGLL" "London Heathrow" OFF \
-        "EGKK" "London Gatwick" OFF \
-        "EGCC" "Manchester" OFF \
-        "EGBB" "Birmingham" OFF \
-        "EGSS" "London Stansted" OFF \
-        "EGPF" "Glasgow" OFF \
-        "EGPH" "Edinburgh" OFF \
-        "EGPD" "Aberdeen Dyce" OFF \
-        "EGPK" "Prestwick" OFF \
-        "EGHH" "Bournemouth" OFF \
-        "EGHI" "Southampton" OFF \
-        "EGNT" "Newcastle" OFF \
-        "EGNX" "East Midlands" OFF \
-        "EGGW" "London Luton" OFF \
-        "EGGD" "Bristol" OFF \
-        "EGCN" "Sheffield" OFF \
-        "EGNM" "Leeds Bradford" OFF \
-        "EGNS" "Ronaldsway" OFF \
-        "EGAA" "Belfast Aldergrove" OFF \
-        "EGBD" "Belfast City" OFF 3>&1 1>&2 2>&3)
-        selected=$(echo "$selected" | sed 's/"//g')
-        selected=$(echo "$selected" | tr ' ' ',')
-        echo $selected
-        
+#!/bin/bash
+
+## Setting up Reflector
