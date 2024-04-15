3360b1a1276c2a9743f23f43bcfd668c8c507b6f
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-15 11:58:12 +0100

    m

--- functions/modulemetar_setup_fr.sh
+++ functions/modulemetar_setup_fr.sh
@@ -55,8 +55,7 @@ selected=$(whiptail --title "Metar Info" --checklist "choose which Airports:" 27
         "LFSL" "Brive Souillac" OFF \
         "LFBE" "Bergerac" OFF 3>&1 1>&2 2>&3)
         specific_airport=$(echo "$specific_airport" | sed's/"//g')
-        sed -i "s/STARTDEFAULT=EDDP/STARTDEFAULT=$specific_airport/g" >> /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
-        sed -i "s/DEFAULT_LANG=en_US/DEFAULT_LANG=$lang/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
+        sed -i "s/\#STARTDEFAULT=EDDP/STARTDEFAULT=$specific_airport/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
         echo -e "$(date)" "${GREEN} $selected Aèroports inclu avec le défaut Aèroport $specific_airport ${NORMAL}" | tee -a /var/log/install.log
    
     else
