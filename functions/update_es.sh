a8c08575870f9b0b6e89a371e948e0e8d5c58fbf
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-24 12:50:30 +0100

    m

--- functions/update_es.sh
+++ functions/update_es.sh
@@ -4,7 +4,7 @@ CONF="/etc/svxlink/svxlink.conf"
 GPIO="/etc/svxlink/gpio.conf"
 OP=/etc/svxlink
 
-whiptail --title "Mis à jour" --msgbox "Mis à jour les répositoires pour nodejs et l'éxpansion pou l'avenire. Tapé OK pour continuer" 8 78
+whiptail --title "Actualizado" --msgbox "Directorios actualizados para nodo js y expansión para el futuro. Escribe OK para continuar" 8 78
 
 	echo -e "$(date)" "${YELLOW}  #### updates and upgrades #### ${NORMAL}" | tee -a  /var/log/install.log
 
@@ -14,7 +14,7 @@ whiptail --title "Mis à jour" --msgbox "Mis à jour les répositoires pour node
 
 	echo -e "$(date)" "${YELLOW}  #### commence build #### ${NORMAL}" | tee -a  /var/log/install.log
 ###### BUILD ESSENTIALS ####
-	whiptail --title "Build Essentials" --msgbox "Addition tous les paquets necessaires pour Svxlink. Tapé OK pour continuer" 8 78
+	whiptail --title "Construir elementos esenciales" --msgbox "Agregue todos los paquetes necesarios para Svxlink. Escribe OK para continuar" 8 78
 
 	echo -e "$(date)" "${YELLOW} ### Installation des paquets ### ${NORMAL}" | tee -a  /var/log/install.log
  	sudo apt-get install build-essential g++ make cmake libsigc++-2.0-dev php8.2 nodejs libgsm1-dev libudev-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial sqlite3 php8.2-sqlite3 toilet -y
