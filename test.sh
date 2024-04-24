508a786a67716a843bf59e878df22f787e22ed89
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-24 20:25:22 +0100

    m

--- test.sh
+++ test.sh
@@ -5,5 +5,6 @@ LANG_OPTION=$(whiptail --title "Language Option" --menu "Select Language" 13 78 
         "3" "English  (USA) en_US" \
         "4" "Espagnol (Espagne) - es_ES" \
          3>&1 1>&2 2>&3 )
+echo $LANG_OPTION
 
 ## Setting up Reflector
