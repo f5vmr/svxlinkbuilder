0468b98dd1d0863ebf55b2f329d0d59729b8181f
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-24 17:52:18 +0100

    m

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 LANG_OPTION=$(whiptail --title "Language Option" --menu "Select Language" 13 78 5 \
-        "1" "English  United Kingdom en_GB" \
-        "2" "Français   France fr_FR" \
-        "3" "English  USA en_US" \
-        "4" "Espagnol (Spain) - es_ES" \
+        "1" "English (United Kingdom) en_GB" \
+        "2" "Français (France) fr_FR" \
+        "3" "English (USA) en_US" \
+        "4" "Espagnol (Espagne) - es_ES" \
          3>&1 1>&2 2>&3 )
 
 ## Setting up Reflector
