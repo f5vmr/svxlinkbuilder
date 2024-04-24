0d4eabf9be7d8c8d18f0d551a8dcc61bd0ad00b8
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-24 17:53:05 +0100

    m

--- functions/language.sh
+++ functions/language.sh
@@ -14,9 +14,9 @@ function set_locale() {
 
 function which_language {
     LANG_OPTION=$(whiptail --title "Language Option" --menu "Select Language" 13 78 5 \
-        "1" "English (United Kingdom) en_GB" \
+        "1" "English  (United Kingdom) en_GB" \
         "2" "Français (France) fr_FR" \
-        "3" "English (USA) en_US" \
+        "3" "English  (USA) en_US" \
         "4" "Espagnol (Espagne) - es_ES" \
          3>&1 1>&2 2>&3 )
 
--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 LANG_OPTION=$(whiptail --title "Language Option" --menu "Select Language" 13 78 5 \
-        "1" "English (United Kingdom) en_GB" \
+        "1" "English  (United Kingdom) en_GB" \
         "2" "Français (France) fr_FR" \
-        "3" "English (USA) en_US" \
+        "3" "English  (USA) en_US" \
         "4" "Espagnol (Espagne) - es_ES" \
          3>&1 1>&2 2>&3 )
 
