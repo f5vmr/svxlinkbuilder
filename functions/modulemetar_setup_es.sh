b827542d0fb3623683c28262ca69ad16b48718c5
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-24 18:45:37 +0100

    m

--- functions/modulemetar_setup_es.sh
+++ functions/modulemetar_setup_es.sh
@@ -6,96 +6,96 @@ whiptail --title "Información del medidor" --yesno "¿Desea configurar este mó
     if [ $? -eq "0" ] 
     then
         sleep 1
-    selected=$(whiptail --title "Información del medidor" --checklist "Elija qué aeropuertos:" 27 78 20 \
-    “LECO” “A Coruña” OFF \
-“LEAB” “Albacete” OFF \
-“LEAL” “Alicante-Elche” OFF \
-“LEAM” “Almeria” OFF \
-“LEAS” “Asterias” OFF \
-“LEBZ” “Badajoz” OFF \
-“LEBL” “Barcelona-El Prat” OFF \
-“LEBB” “Bilbao” OFF \
-“LEBG” “Burgos” OFF \
-“LEBA” “Cordoba” OFF \
-“LEGE” “Castellón-Costa-Azahar” OFF \
-“LEGR” “Federico Garcia Lorca” OFF \
-“LEHC” “Aragon” OFF \
-“LEJR” “Jerez” OFF \
-“LESU” “Andorra-La seu d’Urgell” OFF \
-“LELN” “León” OFF \
-“LEMD” “Madrid-Barajas” OFF \
-“LECU” “Madrid-Cuatros Vientos” OFF \
-“LETO” “Madrid-Torrejón” OFF \
-“LEMG” “Malaga” OFF \
-“LEPP” “Pamplona-Nóain” OFF \
-“LEMI” “Mercia” OFF \
-“LERS” “Reus” OFF \
-“LELL” “Sabadell” OFF \
-“LESA” “Salamanca” OFF \
-“LESO” “Fuenterrabia” OFF \
-“LEXJ” “Santander-Paravas” OFF \
-“LEST” “Santiago de Compostella” OFF \
-“LEZL” “Seville-San Pablo” OFF \
-“LETL” “Teruel” OFF \
-“LEVC” “Valencia” OFF \
-“LEVX” “Vigo-Peindora” OFF \
-“LEVT” “Vitoria-Foronda” OFF \
-“LEZG” “Zaragoza” OFF \
-“LEPA” “Palma” OFF \
-“LEMH” “Menorca” OFF \
-“LEIB” “Ibiza” OFF \
-“GCFV” “Fuetaventura” OFF \
-“GCLP” “Las Palmas GC” OFF \
-“GCTS” “Tenerife Sud” OFF \
-“GCLA” “La Palma” OFF \
-“GCHI” “El Hierro” OFF 3>&1 1>&2 2>&3)
+    selected=$(whiptail --title "Información del medidor" --checklist "Elija qué aeropuertos:" 43 78 20 \
+        "LECO"  "A Coruña " OFF \
+        "LEAB"  "Albacete " OFF \
+        "LEAL"  "Alicante-Elche " OFF \
+        "LEAM"  "Almeria " OFF \
+        "LEAS"  "Asterias " OFF \
+        "LEBZ"  "Badajoz " OFF \
+        "LEBL"  "Barcelona-El Prat " OFF \
+        "LEBB"  "Bilbao " OFF \
+        "LEBG"  "Burgos " OFF \
+        "LEBA"  "Cordoba " OFF \
+        "LEGE"  "Castellón-Costa-Azahar " OFF \
+        "LEGR"  "Federico Garcia Lorca " OFF \
+        "LEHC"  "Aragon " OFF \
+        "LEJR"  "Jerez " OFF \
+        "LESU"  "Andorra-La seu d’Urgell " OFF \
+        "LELN"  "León " OFF \
+        "LEMD"  "Madrid-Barajas " OFF \
+        "LECU"  "Madrid-Cuatros Vientos " OFF \
+        "LETO"  "Madrid-Torrejón " OFF \
+        "LEMG"  "Malaga " OFF \
+        "LEPP"  "Pamplona-Nóain " OFF \
+        "LEMI"  "Mercia " OFF \
+        "LERS"  "Reus " OFF \
+        "LELL"  "Sabadell " OFF \
+        "LESA"  "Salamanca " OFF \
+        "LESO"  "Fuenterrabia " OFF \
+        "LEXJ"  "Santander-Paravas " OFF \
+        "LEST"  "Santiago de Compostella " OFF \
+        "LEZL"  "Seville-San Pablo " OFF \
+        "LETL"  "Teruel " OFF \
+        "LEVC"  "Valencia " OFF \
+        "LEVX"  "Vigo-Peindora " OFF \
+        "LEVT"  "Vitoria-Foronda " OFF \
+        "LEZG"  "Zaragoza " OFF \
+        "LEPA"  "Palma " OFF \
+        "LEMH"  "Menorca " OFF \
+        "LEIB"  "Ibiza " OFF \
+        "GCFV"  "Fuetaventura " OFF \
+        "GCLP"  "Las Palmas GC " OFF \
+        "GCTS"  "Tenerife Sud " OFF \
+        "GCLA"  "La Palma " OFF \
+        "GCHI"  "El Hierro " OFF 3>&1 1>&2 2>&3)
         selected=$(echo "$selected" | sed 's/"//g')
         selected=$(echo "$selected" | tr ' ' ',')
         sed -i "s/AIRPORTS=.*/AIRPORTS=$selected/g"  /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
         
-        specific_airport=$(whiptail --title "Información del medidor" --radiolist "Especifique el código OACI del aeropuerto para un aeropuerto predeterminado: " 27 78 20 \
-        “LECO” “A Coruña” OFF \
-“LEAB” “Albacete” OFF \
-“LEAL” “Alicante-Elche” OFF \
-“LEAM” “Almeria” OFF \
-“LEAS” “Asterias” OFF \
-“LEBZ” “Badajoz” OFF \
-“LEBL” “Barcelona-El Prat” OFF \
-“LEBB” “Bilbao” OFF \
-“LEBG” “Burgos” OFF \
-“LEBA” “Cordoba” OFF \
-“LEGE” “Castellón-Costa-Azahar” OFF \
-“LEGR” “Federico Garcia Lorca” OFF \
-“LEHC” “Aragon” OFF \
-“LEJR” “Jerez” OFF \
-“LESU” “Andorra-La seu d’Urgell” OFF \
-“LELN” “León” OFF \
-“LEMD” “Madrid-Barajas” OFF \
-“LECU” “Madrid-Cuatros Vientos” OFF \
-“LETO” “Madrid-Torrejón” OFF \
-“LEMG” “Malaga” OFF \
-“LEPP” “Pamplona-Nóain” OFF \
-“LEMI” “Mercia” OFF \
-“LERS” “Reus” OFF \
-“LELL” “Sabadell” OFF \
-“LESA” “Salamanca” OFF \
-“LESO” “Fuenterrabia” OFF \
-“LEXJ” “Santander-Paravas” OFF \
-“LEST” “Santiago de Compostella” OFF \
-“LEZL” “Seville-San Pablo” OFF \
-“LETL” “Teruel” OFF \
-“LEVC” “Valencia” OFF \
-“LEVX” “Vigo-Peindora” OFF \
-“LEVT” “Vitoria-Foronda” OFF \
-“LEZG” “Zaragoza” OFF \
-“LEPA” “Palma” OFF \
-“LEMH” “Menorca” OFF \
-“LEIB” “Ibiza” OFF \
-“GCFV” “Fuetaventura” OFF \
-“GCLP” “Las Palmas GC” OFF \
-“GCTS” “Tenerife Sud” OFF \
-“GCLA” “La Palma” OFF \
-“GCHI” “El Hierro” OFF 3>&1 1>&2 2>&3)
+        specific_airport=$(whiptail --title "Información del medidor" --radiolist "Especifique el código OACI del aeropuerto para un aeropuerto predeterminado: " 43 78 20 \
+        "LECO"  "A Coruña " OFF \
+        "LEAB"  "Albacete " OFF \
+        "LEAL"  "Alicante-Elche " OFF \
+        "LEAM"  "Almeria " OFF \
+        "LEAS"  "Asterias " OFF \
+        "LEBZ"  "Badajoz " OFF \
+        "LEBL"  "Barcelona-El Prat " OFF \
+        "LEBB"  "Bilbao " OFF \
+        "LEBG"  "Burgos " OFF \
+        "LEBA"  "Cordoba " OFF \
+        "LEGE"  "Castellón-Costa-Azahar " OFF \
+        "LEGR"  "Federico Garcia Lorca " OFF \
+        "LEHC"  "Aragon " OFF \
+        "LEJR"  "Jerez " OFF \
+        "LESU"  "Andorra-La seu d’Urgell " OFF \
+        "LELN"  "León " OFF \
+        "LEMD"  "Madrid-Barajas " OFF \
+        "LECU"  "Madrid-Cuatros Vientos " OFF \
+        "LETO"  "Madrid-Torrejón " OFF \
+        "LEMG"  "Malaga " OFF \
+        "LEPP"  "Pamplona-Nóain " OFF \
+        "LEMI"  "Mercia " OFF \
+        "LERS"  "Reus " OFF \
+        "LELL"  "Sabadell " OFF \
+        "LESA"  "Salamanca " OFF \
+        "LESO"  "Fuenterrabia " OFF \
+        "LEXJ"  "Santander-Paravas " OFF \
+        "LEST"  "Santiago de Compostella " OFF \
+        "LEZL"  "Seville-San Pablo " OFF \
+        "LETL"  "Teruel " OFF \
+        "LEVC"  "Valencia " OFF \
+        "LEVX"  "Vigo-Peindora " OFF \
+        "LEVT"  "Vitoria-Foronda " OFF \
+        "LEZG"  "Zaragoza " OFF \
+        "LEPA"  "Palma " OFF \
+        "LEMH"  "Menorca " OFF \
+        "LEIB"  "Ibiza " OFF \
+        "GCFV"  "Fuetaventura " OFF \
+        "GCLP"  "Las Palmas GC " OFF \
+        "GCTS"  "Tenerife Sud " OFF \
+        "GCLA"  "La Palma " OFF \
+        "GCHI"  "El Hierro " OFF 3>&1 1>&2 2>&3)
         specific_airport=$(echo "$specific_airport" | sed 's/"//g')
         sed -i "s/\#STARTDEFAULT=EDDP/STARTDEFAULT=$specific_airport/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
         echo -e "$(date)" "${GREEN} $selected Aeropuertos incluidos con el aeropuerto predeterminado $specific_airport ${NORMAL}" | tee -a /var/log/install.log
