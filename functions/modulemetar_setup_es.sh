84c21b4f2165065af23dab38e1855fee9a1fb638
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-24 14:18:49 +0100

    m

--- functions/modulemetar_setup_es.sh
+++ functions/modulemetar_setup_es.sh
@@ -99,7 +99,7 @@ whiptail --title "Información del medidor" --yesno "¿Desea configurar este mó
 “GCHI” “El Hierro” OFF 3>&1 1>&2 2>&3)
         specific_airport=$(echo "$specific_airport" | sed 's/"//g')
         sed -i "s/\#STARTDEFAULT=EDDP/STARTDEFAULT=$specific_airport/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
-        echo -e "$(date)" "${GREEN} $selected Airports included with default Airport $specific_airport ${NORMAL}" | tee -a /var/log/install.log
+        echo -e "$(date)" "${GREEN} $selected Aeropuertos incluidos con el aeropuerto predeterminado $specific_airport ${NORMAL}" | tee -a /var/log/install.log
    
     else
      sed -i 's/,ModuleMetarInfo//' /etc/svxlink/svxlink.conf
