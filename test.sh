88f6236bb4bd0c499282ce46bd22d305e86ec2c0
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-24 20:44:49 +0100

    m

--- test.sh
+++ test.sh
@@ -1,13 +1,93 @@
 #!/bin/bash
-LANG_OPTION=$(whiptail --title "Language Option" --menu "Select Language" 13 78 5 \
-        "1" "English  (United Kingdom) en_GB" \
-        "2" "Français (France) fr_FR" \
-        "3" "English  (USA) en_US" \
-        "4" "Espagnol (Espagne) - es_ES" \
-         3>&1 1>&2 2>&3 )
-if [[ "$LANG_OPTION" == "4" ]]; then
-echo hooray
-fi
-
+selected=$(whiptail --title "Información del medidor" --checklist "Elija qué aeropuertos:" 43 78 43 \
+        "LECO"  "A Coruña " OFF \
+        "LEAB"  "Albacete " OFF \
+        "LEAL"  "Alicante-Elche " OFF \
+        "LEAM"  "Almeria " OFF \
+        "LEAS"  "Asterias " OFF \
+        "LEBZ"  "Badajoz " OFF \
+        "LEBL"  "Barcelona-El Prat " OFF \
+        "LEBB"  "Bilbao " OFF \
+        "LEBG"  "Burgos " OFF \
+        "LEBA"  "Cordoba " OFF \
+        "LEGE"  "Castellón-Costa-Azahar " OFF \
+        "LEGR"  "Federico Garcia Lorca " OFF \
+        "LEHC"  "Aragon " OFF \
+        "LEJR"  "Jerez " OFF \
+        "LESU"  "Andorra-La seu d’Urgell " OFF \
+        "LELN"  "León " OFF \
+        "LEMD"  "Madrid-Barajas " OFF \
+        "LECU"  "Madrid-Cuatros Vientos " OFF \
+        "LETO"  "Madrid-Torrejón " OFF \
+        "LEMG"  "Malaga " OFF \
+        "LEPP"  "Pamplona-Nóain " OFF \
+        "LEMI"  "Mercia " OFF \
+        "LERS"  "Reus " OFF \
+        "LELL"  "Sabadell " OFF \
+        "LESA"  "Salamanca " OFF \
+        "LESO"  "Fuenterrabia " OFF \
+        "LEXJ"  "Santander-Paravas " OFF \
+        "LEST"  "Santiago de Compostella " OFF \
+        "LEZL"  "Seville-San Pablo " OFF \
+        "LETL"  "Teruel " OFF \
+        "LEVC"  "Valencia " OFF \
+        "LEVX"  "Vigo-Peindora " OFF \
+        "LEVT"  "Vitoria-Foronda " OFF \
+        "LEZG"  "Zaragoza " OFF \
+        "LEPA"  "Palma " OFF \
+        "LEMH"  "Menorca " OFF \
+        "LEIB"  "Ibiza " OFF \
+        "GCFV"  "Fuetaventura " OFF \
+        "GCLP"  "Las Palmas GC " OFF \
+        "GCTS"  "Tenerife Sud " OFF \
+        "GCLA"  "La Palma " OFF \
+        "GCHI"  "El Hierro " OFF 3>&1 1>&2 2>&3)
+        selected=$(echo "$selected" | sed 's/"//g')
+        selected=$(echo "$selected" | tr ' ' ',')
+        sed -i "s/AIRPORTS=.*/AIRPORTS=$selected/g"  /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
+        
+        specific_airport=$(whiptail --title "Información del medidor" --radiolist "Especifique el código OACI del aeropuerto para un aeropuerto predeterminado: " 43 78 20 \
+        "LECO"  "A Coruña " OFF \
+        "LEAB"  "Albacete " OFF \
+        "LEAL"  "Alicante-Elche " OFF \
+        "LEAM"  "Almeria " OFF \
+        "LEAS"  "Asterias " OFF \
+        "LEBZ"  "Badajoz " OFF \
+        "LEBL"  "Barcelona-El Prat " OFF \
+        "LEBB"  "Bilbao " OFF \
+        "LEBG"  "Burgos " OFF \
+        "LEBA"  "Cordoba " OFF \
+        "LEGE"  "Castellón-Costa-Azahar " OFF \
+        "LEGR"  "Federico Garcia Lorca " OFF \
+        "LEHC"  "Aragon " OFF \
+        "LEJR"  "Jerez " OFF \
+        "LESU"  "Andorra-La seu d’Urgell " OFF \
+        "LELN"  "León " OFF \
+        "LEMD"  "Madrid-Barajas " OFF \
+        "LECU"  "Madrid-Cuatros Vientos " OFF \
+        "LETO"  "Madrid-Torrejón " OFF \
+        "LEMG"  "Malaga " OFF \
+        "LEPP"  "Pamplona-Nóain " OFF \
+        "LEMI"  "Mercia " OFF \
+        "LERS"  "Reus " OFF \
+        "LELL"  "Sabadell " OFF \
+        "LESA"  "Salamanca " OFF \
+        "LESO"  "Fuenterrabia " OFF \
+        "LEXJ"  "Santander-Paravas " OFF \
+        "LEST"  "Santiago de Compostella " OFF \
+        "LEZL"  "Seville-San Pablo " OFF \
+        "LETL"  "Teruel " OFF \
+        "LEVC"  "Valencia " OFF \
+        "LEVX"  "Vigo-Peindora " OFF \
+        "LEVT"  "Vitoria-Foronda " OFF \
+        "LEZG"  "Zaragoza " OFF \
+        "LEPA"  "Palma " OFF \
+        "LEMH"  "Menorca " OFF \
+        "LEIB"  "Ibiza " OFF \
+        "GCFV"  "Fuetaventura " OFF \
+        "GCLP"  "Las Palmas GC " OFF \
+        "GCTS"  "Tenerife Sud " OFF \
+        "GCLA"  "La Palma " OFF \
+        "GCHI"  "El Hierro " OFF 3>&1 1>&2 2>&3)
 
 ## Setting up Reflector
