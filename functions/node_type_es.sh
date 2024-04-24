25f157c4c79b3e3904540b2f39287629e9e462c8
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-24 15:16:23 +0100

    m

--- functions/node_type_es.sh
+++ functions/node_type_es.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 node=""
 function nodeoption {
-    NODE_OPTION=$(whiptail --title "Svxlink" --menu "SVP choisir le type de noed vous desirez?" 12 78 5 \
-            "1" " Simplex Noed sans SvxReflecteur" \
-            "2" " Simplex Noed avec SvxReflecteur" \
-            "3" "Repeater Noed sans SvxReflecteur" \
-            "4" "Repeater Noed avec SvxReflecteur" 3>&1 1>&2 2>&3)
+    NODE_OPTION=$(whiptail --title "Svxlink" --menu "¿Elija el tipo de nodo que desea?" 12 78 5 \
+            "1" " Nodo simplex sin SvxReflector" \
+            "2" " Nodo simplex con SvxReflector" \
+            "3" "Nodo repetidor sin SvxReflector" \
+            "4" "Nodo repetidor con SvxReflector" 3>&1 1>&2 2>&3)
 
     if [ "$NODE_OPTION" -eq "1" ] 
     then
-        echo "Choisi Noed Simplex sans SvxReflecteur" | tee -a /var/log/install.log
+        echo "Elija Nodo Simplex sin SvxReflector" | tee -a /var/log/install.log
     elif [ "$NODE_OPTION" -eq "2" ] 
     then
-        echo "Choisi Noed Simplex avec SvxReflecteur" | tee -a /var/log/install.log
+        echo "Elija Nodo Simplex con SvxReflector" | tee -a /var/log/install.log
     elif [ "$NODE_OPTION" -eq "3" ] 
     then
-        echo "Choisi Noed Répéteur sans SvxReflecteur" | tee -a /var/log/install.log
+        echo "Elija Nodo repetidor sin SvxReflector" | tee -a /var/log/install.log
     elif [ "$NODE_OPTION" -eq "4" ] 
     then
-        echo "Choisi Noed Répéteur avec SvxReflecteur" | tee -a /var/log/install.log
+        echo "Elija Nodo repetidor con SvxReflector" | tee -a /var/log/install.log
     else 
-        echo "N'aucune option choisie" | tee -a /var/log/install.log
+        echo "Ninguna opción elegida" | tee -a /var/log/install.log
 fi
 echo Node Option $NODE_OPTION
 }
