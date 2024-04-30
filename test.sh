e489e75b2a1ffb41ffa456e99cf38066dfb74f08
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-30 19:10:23 +0100

    m

--- test.sh
+++ test.sh
@@ -63,7 +63,6 @@ short_voice_id_enable=$(head -n 40 "$logicfile" | awk '/^variable short_voice_id
 short_cw_id_enable=$(head -n 40 "$logicfile" | awk '/^variable short_cw_id_enable/{print $NF; exit}')
 long_voice_id_enable=$(head -n 40 "$logicfile" | awk '/^variable long_voice_id_enable/{print $NF; exit}')
 long_cw_id_enable=$$(head -n 40 "$logicfile" | awk '/^variable long_cw_id_enable/{print $NF; exit}')
-echo "$short_voice_id_enable"
 
 
 #
