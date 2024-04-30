3ca8a522caefd1b7368845b5866b17eaf3c0b154
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-30 20:20:07 +0100

    m

--- test.sh
+++ test.sh
@@ -98,10 +98,17 @@ echo "Long CW ID Enable: $new_long_cw_id_enable"
 
 
 # Update the Logic.tcl file with the new values
-sed -i "s/variable short_voice_id_enable\s*\K.*/$new_short_voice_id_enable/g" "$logicfile"
-sed -i "s/variable short_cw_id_enable\s*\K.*/$new_short_cw_id_enable/g" "$logicfile"
-sed -i "s/variable long_voice_id_enable\s*\K.*/$new_long_voice_id_enable/g" "$logicfile"
-sed -i "s/variable long_cw_id_enable\s*\K.*/$new_long_cw_id_enable/g" "$logicfile"
+echo "sed command for short_voice_id_enable:"
+echo "sed -i 's/variable short_voice_id_enable\s*\K.*/$new_short_voice_id_enable/g' $logicfile"
+
+echo "sed command for short_cw_id_enable:"
+echo "sed -i 's/variable short_cw_id_enable\s*\K.*/$new_short_cw_id_enable/g' $logicfile"
+
+echo "sed command for long_voice_id_enable:"
+echo "sed -i 's/variable long_voice_id_enable\s*\K.*/$new_long_voice_id_enable/g' $logicfile"
+
+echo "sed command for long_cw_id_enable:"
+echo "sed -i 's/variable long_cw_id_enable\s*\K.*/$new_long_cw_id_enable/g' $logicfile"
 
 
 # Extract the content of the send_rgr_sound procedure
