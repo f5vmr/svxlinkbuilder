ae25cf9d61deaf314a4f6c99870e7149d87f9fa0
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-05-01 10:08:27 +0100

    m

--- functions/deletion.sh
+++ functions/deletion.sh
@@ -12,11 +12,6 @@ fi
 
 # Use sed to delete lines between [SimplexLogic] and the next section name
 sed -i "/^\[$section_name\]/,/^ *\[/ {/^\[$section_name\]/b; /^\[/b; d}" "$svxconf_file"
-
-
-
-
-
 echo "Section $section_name deleted from $svxconf_file" | tee -a /var/log/install.log
 
 
