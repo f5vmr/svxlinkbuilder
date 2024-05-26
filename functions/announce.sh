0e4dccfb0e585ea4ec587cf9b85de0b5cf2292a1
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-05-26 15:08:31 +0100

    m

--- functions/announce.sh
+++ functions/announce.sh
@@ -40,7 +40,7 @@ echo "Replacing CW_AMP with $new_cw_amp"
 # Update svxlink.conf with the new values for CW_AMP
 # Update svxlink.conf with the new values for CW_AMP using a different delimiter
 # Update svxlink.conf with the new values for CW_AMP with the replacement value enclosed in double quotes
-#sed -i "s/^CW_AMP=.*/CW_AMP=\"$new_cw_amp\"/g" -- "$svxconf_file"
+#sed -i "s/CW_AMP=.*/CW_AMP=\"$new_cw_amp\"/g" -- "$svxconf_file"
 # Update svxlink.conf with the new value for CW_AMP using awk
 sed -i "s/CW_AMP=.*/CW_AMP=$new_cw_amp/g" "$svxconf_file"
 echo "Replacing CW_PITCH with $new_cw_pitch"
