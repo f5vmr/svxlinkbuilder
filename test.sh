d269d623bb9d931d9ba5c8adb08d7c2dbfee5019
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-06-03 20:03:33 +0100

    m

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ logictcl="Logic.tcl"
 logicfile="$LOGIC_DIR/$logictcl"
 svxconf_file="$CONF_DIR/$svxfile"
 ##### Adding a ReflectorLogic section to svxlink.conf #####
-if [[ "$NODE_OPTION" == "2" ]] || [[ "$NODE_OPTION" == "4" ]]; then 
+#if [[ "$NODE_OPTION" == "2" ]] || [[ "$NODE_OPTION" == "4" ]]; then 
     section_name="ReflectorLogic"
 
     # Check if the section already exists
@@ -18,4 +18,5 @@ echo "Section $section_name already exists in $svxconf_file" | tee -a /var/log/i
     echo "no valid option"
 
     fi
+ #
     fi
