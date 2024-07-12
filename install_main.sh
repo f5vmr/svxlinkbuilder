2499ca6862372ed3d3e42451b3b3a28565ec2de7
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-07-12 09:05:01 +0100

    Update Includes 3-min timeout

--- install_main.sh
+++ install_main.sh
@@ -82,6 +82,7 @@ chmod 0440 "$SUDOERS_FILE"
 	sudo mkdir /home/pi/scripts
 	sudo cp -f /home/pi/svxlinkbuilder/addons/10-uname /etc/update-motd.d/
  	sudo cp -f /home/pi/svxlinkbuilder/configs/svxlink.conf /etc/svxlink/
+	sudo cp -f /home/pi/svxlinkbuilder/configs/Logic.tcl /usr/share/svxlink/events.d/local/Logic.tcl
  	sudo cp -f /home/pi/svxlinkbuilder/addons/node_info.json /etc/svxlink/node_info.json
  	sudo cp -f /home/pi/svxlinkbuilder/resetlog.sh /home/pi/scripts/resetlog.sh
  	(sudo crontab -l 2>/dev/null; echo "59 23 * * * /home/pi/scripts/resetlog.sh ") | sudo crontab -
