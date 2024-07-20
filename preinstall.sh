c28ce47c60ff858873f20258b5761a81b8a1a9d3
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-07-20 21:16:24 +0100

    small update

--- preinstall.sh
+++ preinstall.sh
@@ -48,7 +48,7 @@
     sudo apt update && sudo apt upgrade -y
     ## Install svxlink-server  and dependencies ##
     echo -e "${BLUE}#### Installing svxlink-server ####${WHITE}" | sudo tee -a /var/log/install.log
-    sudo apt install -y svxlink-server apache2 apache2-bin apache2-data apache2-utils php8.2 python3-serial sqlite3 libssl-dev php8.2-sqlite3 toilet libgpiod-dev -y
+    sudo apt install -y curl svxlink-server apache2 apache2-bin apache2-data apache2-utils php8.2 python3-serial sqlite3 libssl-dev php8.2-sqlite3 toilet libgpiod-dev --fix-missing -y
     echo -e "${BLUE}#### Installing locales ####${WHITE}" | sudo tee -a /var/log/install.log
     
     ## Must kill the remotetrx.service to avoid a problem later ##
