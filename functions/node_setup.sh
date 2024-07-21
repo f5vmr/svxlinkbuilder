be3b0ae36141e95a9885915c838b982b91ff9271
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-07-21 10:51:29 +0100

    Message

--- functions/node_setup.sh
+++ functions/node_setup.sh
@@ -13,7 +13,7 @@ function nodeset {
     then
     node="Simplex with UK Svxreflector"
     auth_key=$(whiptail --passwordbox "Please enter your SvxReflector Key" 8 78 --title "password dialog" 3>&1 1>&2 2>&3)
-     sed -i "s/AUTH_KEY=\"GET YOUR OWN KEY\"/AUTH_KEY=\"$auth_key\"/g" /etc/svxlink/svxlink.conf 
+     sed -i "s/AUTH_KEY=\"Change this key now!\"/AUTH_KEY=\"$auth_key\"/g" /etc/svxlink/svxlink.conf 
     elif [[ $NODE_OPTION  == "3" ]] 
     then
     node="Repeater without Svxreflector"
@@ -27,7 +27,7 @@ function nodeset {
     
      sed -i 's/set for SimplexLogic/set for RepeaterLogic/g' /etc/svxlink/svxlink.conf
      sed -i 's/LOGICS=SimplexLogic/LOGICS=RepeaterLogic/g' /etc/svxlink/svxlink.conf
-     sed -i "s/AUTH_KEY=\"GET YOUR OWN KEY\"/AUTH_KEY=\"$auth_key\"/g" /etc/svxlink/svxlink.conf 
+     sed -i "s/AUTH_KEY=\"Change this key now!\"/AUTH_KEY=\"$auth_key\"/g" /etc/svxlink/svxlink.conf 
     else    
     node="unset"
     fi  
