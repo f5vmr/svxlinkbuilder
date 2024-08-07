3996d4d063918bb0d23092ba12dd2c68cf1ed186
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-08-07 20:11:16 +0100

    update svxlink.conf - OPEN_ON_CTCSS

--- functions/sa818_prog.sh
+++ functions/sa818_prog.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+function sa818_prog {
 #### This is for the additions of the udracard.sh script to the svxlink.conf file
+band = (whiptail --title "SA818 Device Check" -- "Do you have an SA818 device fitted as a transceiver in your hotspot? If you do not then select 'No' otherwise select 'Yes'." 10 78); then
+    sa818=true
+else
+    sa818=false
+fi
+#### Band checking ####
+
+
 #### Changing Frequency ####
+}
---
+++ functions/sa818_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+function sa818_test {
+if (whiptail --title "SA818 Device Check" --defaultno --yesno "Do you have an SA818 device fitted as a transceiver in your hotspot? If you do not then select 'No' otherwise select 'Yes'." 10 78); then
+    sa818=true
+else
+    sa818=false
+fi
+
+# Output the result for verification
+echo "SA818 device fitted: $sa818" | sudo tee -a /var/log/install.log
+}
