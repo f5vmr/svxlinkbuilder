890d2fd6d9d910708d684875dde4bdba4884e7d6
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-08-07 19:44:38 +0100

    test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Check if the SA818 device is fitted
-if (whiptail --title "SA818 Device Check" --yesno "Do you have an SA818 device fitted as a transceiver in your hotspot? If you do not then select 'No' otherwise select 'Yes'." 10 78); then
+
+if (whiptail --title "SA818 Device Check" --defaultno --yesno "Do you have an SA818 device fitted as a transceiver in your hotspot? If you do not then select 'No' otherwise select 'Yes'." 10 78); then
     sa818=true
 else
     sa818=false
