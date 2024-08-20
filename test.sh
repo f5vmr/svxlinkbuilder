9e7c4ac93f565480ca5de321f5c200f7b96bab05
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-08-20 21:04:28 +0100

    sa818

--- test.sh
+++ test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 #Calling the functions.
+source "${BASH_SOURCE%/*}/functions/sa818_test.sh"
+sa818_test
+if [[ $sa818 == true ]]; then
 source "${BASH_SOURCE%/*}/functions/sa818_menu.sh"
 sa818_menu
-#source "${BASH_SOURCE%/*}/functions/sa818_prog.sh"
-#sa818_prog
-
-echo "$band SA818 device fitted and programmed to $selected_frequency "
-
+fi
 
