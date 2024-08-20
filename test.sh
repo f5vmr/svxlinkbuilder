f04f05032b7337d0884d3094c6d8948cebfc7c6f
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-08-20 20:46:02 +0100

    sa818 test

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 #Calling the functions.
 source "${BASH_SOURCE%/*}/functions/sa818_menu.sh"
 sa818_menu
-source "${BASH_SOURCE%/*}/functions/sa818_prog.sh"
-sa818_prog
+#source "${BASH_SOURCE%/*}/functions/sa818_prog.sh"
+#sa818_prog
 
 echo "$band SA818 device fitted and programmed to $selected_frequency "
 
