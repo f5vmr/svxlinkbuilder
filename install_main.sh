23d425916110b3fadab522e606d363fe81db6597
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-08-05 21:53:55 +0100

    update

--- install_main.sh
+++ install_main.sh
@@ -33,7 +33,7 @@ echo -e "$(date)" "${YELLOW} #### Sound Card: $HID $GPIOD $card #### ${NORMAL}" 
 echo -e "$(date)" "${YELLOW} #### Checking Alsa #### ${NORMAL}" | sudo tee -a  /var/log/install.log
 if {{$NODEOPTION === 1 or $NODEOPTION === 2}}; 
 NODE="SimplexLogic.conf";
-else {{$NODEOPTION === 3 or $NODEOPTION === 4}}; 
+elif {{$NODEOPTION === 3 or $NODEOPTION === 4}}; 
 NODE="RepeaterLogic.conf";
 fi
 #### REQUEST CALLSIGN ####
