1cdf4ccef4a875aa4ac5420b281281abb6a6a22f
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-08-05 15:01:04 +0100

    update

--- functions/svxlink.sh
+++ functions/svxlink.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-URL="https://github.com/f5vmr/svxlink/releases/latest/download/svxlink-25.05.1-Linux.deb"
+URL="https://github.com/f5vmr/svxlink/releases/download/v25.05.1.24/svxlink-V25.05.01.24.g6b56a264-Linux.deb"
 wget -O /tmp/svxlink.deb "$URL"
 sudo apt install -y /tmp/svxlink.deb
