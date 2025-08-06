3888b9e684a43b4f7605f83cbbdb4f0ffa002aa3
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-08-06 09:55:51 +0100

    update

--- preinstall.sh
+++ preinstall.sh
@@ -43,9 +43,14 @@
     install
     ## Install svxlink-server  and dependencies ##
     #echo -e "${BLUE}#### Installing svxlink-server #### ${WHITE}" | sudo tee -a /var/log/install.log
-    sudo apt install -y curl qtel apache2 apache2-bin apache2-data apache2-utils php8.2 python3-serial sqlite3 libssl-dev php8.2-sqlite3 toilet libgpiod-dev --fix-missing -y
+    sudo apt install -y curl qtel apache2 apache2-bin apache2-data apache2-utils php8.2 python3-serial sqlite3 libssl-dev php8.2-sqlite3 toilet libgpiod-dev libgcrypt-dev libgsm1-dev libsigc++-2.0-dev tcl-dev libspeex-dev libasound2-dev libpopt-dev libssl-dev libopus-dev --fix-missing
+    ## Install repository ##
+    source "${BASH_SOURCE%/*}/functions/svxlink.sh"
+    install
+     ## installing locales.##
     echo -e "${BLUE}#### Installing locales #### ${WHITE}" | sudo tee -a /var/log/install.log
-    ## installing locales.##
+    
+   
     ## Set en_GB.UTF-8 as the default locale to begin with.##
     sudo localectl set-locale LANG=en_GB.UTF-8
     echo -e "${CYAN}Locale setup completed.${WHITE}"
