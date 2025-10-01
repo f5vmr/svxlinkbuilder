f1dc1e009ff51ea97f0244d2d3c89a556612aa49
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-10-01 10:51:58 +0100

    update service demand

--- install_main.sh
+++ install_main.sh
@@ -189,7 +189,7 @@ chmod 0440 "$SUDOERS_FILE"
 	 # clear
 	echo -e "$(date)" "${RED} #### Restarting svxlink.service #### ${NORMAL}" | sudo tee -a  /var/log/install.log
 	
- 	sudo systemctl restart svxlink.service
+ 	sudo systemctl enable svxlink --now
 
 	##.service isn't necessary ##
 echo -e "$(date)" "${GREEN} #### Installation complete #### ${NORMAL}" | sudo tee -a  /var/log/install.log
