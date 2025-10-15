7ee7992da6e8a3875a69f4fef1b7ef4c460b51c7
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-10-15 13:16:29 +0100

    update version

--- preinstall.sh
+++ preinstall.sh
@@ -43,9 +43,9 @@
     sudo useradd -rG audio,plugdev,gpio,dialout svxlink
     sudo apt update && sudo apt upgrade -y
    
-    sudo wget -O /var/cache/apt/archives/svxlink-master-Linux.deb \
-    https://github.com/f5vmr/svxlink/releases/download/V25.5/svxlink-master-Linux.deb
-    sudo apt install /var/cache/apt/archives/svxlink-master-Linux.deb -y
+    sudo wget -O /var/cache/apt/archives/svxlink-25.5.1.gb89468d3-Linux.deb \
+    https://github.com/f5vmr/svxlink/releases/download/V25.5/svxlink-25.5.1.gb89468d3-Linux.deb
+    sudo apt install /var/cache/apt/archives/svxlink-25.5.1.gb89468d3-Linux.deb -y
 
     echo -e "${BLUE}#### Installing svxlink #### ${WHITE}" | sudo tee -a /var/log/install.log
     sudo apt install -y curl apache2 apache2-bin apache2-data apache2-utils php8.2 python3-serial toilet --fix-missing -y
