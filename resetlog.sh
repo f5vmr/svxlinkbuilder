971d78521e937011c72e0ee256bc4a93f96ab72e
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-10-31 20:01:18 +0000

    update

--- resetlog.sh
+++ resetlog.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-cd /var/log
-NOW=$(date -d "Today" +%Y%m%d)
-NOW7=$(date -d "Last-Week" +%Y%m%d)
-sleep 7
-cp svxlink.log svxlink.$NOW.txt
-truncate -s0 svxlink.log
-sudo rm svxlink.$NOW7.txt
-
+# Go to the log directory
+cd /var/log || exit 1
+# Timestamps
+NOW=$(date +%Y%m%d)
+NOW7=$(date -d "7 days ago" +%Y%m%d)
+# Backup the current log
+cp svxlink.log svxlink."$NOW".txt
+# Safely truncate the live log
+: > svxlink.log
+# Remove backups older than 7 days
+rm -f svxlink."$NOW7".txt
 
