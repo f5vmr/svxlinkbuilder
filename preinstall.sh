2b6524d0fc452eac6ced6dab1bbe840d0bab9997
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-07 11:04:10 +0000

    updae install - nodejs npm

--- preinstall.sh
+++ preinstall.sh
@@ -48,7 +48,7 @@
     sudo apt install /var/cache/apt/archives/svxlink-25.5.2.g58b8ea4a-Linux.deb -y
 
     echo -e "${BLUE}#### Installing svxlink #### ${WHITE}" | sudo tee -a /var/log/install.log
-    sudo apt install -y curl apache2 apache2-bin apache2-data apache2-utils php8.2 python3-serial toilet --fix-missing -y
+    sudo apt install -y curl apache2 apache2-bin apache2-data apache2-utils php8.2 python3-serial nodejs npm toilet --fix-missing -y
     echo -e "${BLUE}#### Installing locales #### ${WHITE}" | sudo tee -a /var/log/install.log
     
     # Must enable the service.service to avoid a problem later ##
