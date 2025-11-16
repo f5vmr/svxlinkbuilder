217c687adc591cbd2e62baf08eb6fa25c23f3777
maletazul <luissilva@maletazul.pt>
2025-11-16 15:08:28 +0000

    Update sa818_test_pt.sh

--- functions/sa818_test_pt.sh
+++ functions/sa818_test_pt.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+function sa818_test {
+if (whiptail --title "Verificação do dispositivo SA818" --defaultno --yesno "Tem um dispositivo SA818 instalado como transceptor no seu hotspot? Se não tiver, selecione 'Não'. Caso tenha, selecione 'Sim'." 10 78); then
+    sa818=true
+else
+    sa818=false
+fi
 
+}
