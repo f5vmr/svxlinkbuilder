627bab57c50917aa8b8e1ad5de847687a8569413
maletazul <luissilva@maletazul.pt>
2025-11-16 14:37:44 +0000

    Update modulemetar_setup_pt.sh

--- functions/modulemetar_setup_pt.sh
+++ functions/modulemetar_setup_pt.sh
@@ -1,1 +1,63 @@
-
+#!/bin/bash
+#### Metar Info ####
+function modulemetar {
+ 
+whiptail --title "Metar Info" --yesno "Queres configurar este modulo?" 8 78  3>&1 1>&2 2>&3
+    if [ $? -eq "0" ] 
+    then
+        sleep 1
+    selected=$(whiptail --title "Metar Info" --checklist "Escolhe os Aeroportos: Podes alterar tambem no Painel" 24 78 17 \
+        "EGLL" "London Heathrow" OFF \
+        "EGKK" "London Gatwick" OFF \
+        "EGCC" "Manchester" OFF \
+        "EGBB" "Birmingham" OFF \
+        "EGSS" "London Stansted" OFF \
+        "EGPF" "Glasgow" OFF \
+        "EGPH" "Edinburgh" OFF \
+        "EGPD" "Aberdeen Dyce" OFF \
+        "EGPK" "Prestwick" OFF \
+        "EGHH" "Bournemouth" OFF \
+        "EGHI" "Southampton" OFF \
+        "EGNT" "Newcastle" OFF \
+        "EGNX" "East Midlands" OFF \
+        "EGGW" "London Luton" OFF \
+        "EGGD" "Bristol" OFF \
+        "EGCN" "Sheffield" OFF \
+        "EGNM" "Leeds Bradford" OFF \
+        "EGNS" "Ronaldsway" OFF \
+        "EGAA" "Belfast Aldergrove" OFF \
+        "EGBD" "Belfast City" OFF 3>&1 1>&2 2>&3)
+        selected=$(echo "$selected" | sed 's/"//g')
+        selected=$(echo "$selected" | tr ' ' ',')
+        sed -i "s/AIRPORTS=.*/AIRPORTS=$selected/g"  /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
+        sleep 5
+    specific_airport=$(whiptail --title "Metar Info" --radiolist "Escolhe o codigo ICAO para o Aeroporto por defeito: " 24 78 17 \
+        "EGLL" "London Heathrow" OFF \
+        "EGKK" "London Gatwick" OFF \
+        "EGCC" "Manchester" OFF \
+        "EGBB" "Birmingham" OFF \
+        "EGSS" "London Stansted" OFF \
+        "EGPF" "Glasgow" OFF \
+        "EGPH" "Edinburgh" OFF \
+        "EGPD" "Aberdeen Dyce" OFF \
+        "EGPK" "Prestwick" OFF \
+        "EGHH" "Bournemouth" OFF \
+        "EGHI" "Southampton" OFF \
+        "EGNT" "Newcastle" OFF \
+        "EGNX" "East Midlands" OFF \
+        "EGGW" "London Luton" OFF \
+        "EGGD" "Bristol" OFF \
+        "EGCN" "Sheffield" OFF \
+        "EGNM" "Leeds Bradford" OFF \
+        "EGNS" "Ronaldsway" OFF \
+        "EGAA" "Belfast Aldergrove" OFF \
+        "EGBD" "Belfast City" OFF 3>&1 1>&2 2>&3)
+        specific_airport=$(echo "$specific_airport" | sed 's/"//g')
+        sed -i "s/\STARTDEFAULT=.*/STARTDEFAULT=$specific_airport/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
+        echo -e "$(date)" "${GREEN} $selected Aeroportos incluidos no aeroporto por defeito $specific_airport ${NORMAL}" | sudo tee -a /var/log/install.log
+   
+    else
+     sed -i 's/,ModuleMetarInfo//' /etc/svxlink/svxlink.conf
+    # removing MetarInfo from the MODULES= line in both Simplex and Duplex
+    fi
+}
