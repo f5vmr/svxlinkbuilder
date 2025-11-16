d09eca65135376721a0aa02effeebce6dcb54b8a
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-16 21:37:33 +0000

    update

--- functions/reflector.sh
+++ functions/reflector.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 ## Choice of svxlink reflector based on country
 #function reflector {
+# Reflector Name : Reflector Type [1or2or3]:PortNumber:PWD
+# Example: portal.svxlink.uk   : 3 : 5300:Null
+# Example: north.america.svxlink.net : 3 : 5300:Null
+# Example: reflector1.f4ipa.fr : 1 : 5300:517388
+# so when the user wishes to register to a reflector we can provide a list based on country
+
