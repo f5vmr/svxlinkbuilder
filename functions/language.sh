42efcae052e3240717d26681fccd9f8194294c45
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-19 13:30:45 +0000

    update debugging

--- functions/language.sh
+++ functions/language.sh
@@ -18,7 +18,7 @@ function set_locale() {
 function which_language {
     echo "Debugging Language Selection Menu"
     LANG_OPTION=$(whiptail --title "Language Option" \
-    --menu "\nSelect Language" 13 78 7 \
+    --menu "\nSelect Language\n" 13 78 7 \
     "1" "English  (United Kingdom)  en_GB" \
     "2" "Français (France)          fr_FR" \
     "3" "English  (USA)             en_US" \
