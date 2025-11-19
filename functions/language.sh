21715b4f2c57ed4048f07b600155a1eb5213f059
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-19 13:28:35 +0000

    debugging

--- functions/language.sh
+++ functions/language.sh
@@ -17,16 +17,16 @@ function set_locale() {
 
 function which_language {
     echo "Debugging Language Selection Menu"
-    LANG_OPTION=$(whiptail --title "Language Option" --menu "Select Language" 13 78 7 \
-        \
-        "1" "English  (United Kingdom)  en_GB" \
-        "2" "Français (France)          fr_FR" \
-        "3" "English  (USA)             en_US" \
-        "4" "Spanish  (Español)         es_ES" \
-        "5" "Portuguese (Português)     pt_PT" \
-        "6" "Italian  (Italiano)        it_IT" \
-        
-         3>&1 1>&2 2>&3 )
+    LANG_OPTION=$(whiptail --title "Language Option" \
+    --menu "\nSelect Language" 13 78 7 \
+    "1" "English  (United Kingdom)  en_GB" \
+    "2" "Français (France)          fr_FR" \
+    "3" "English  (USA)             en_US" \
+    "4" "Spanish  (Español)         es_ES" \
+    "5" "Portuguese (Português)     pt_PT" \
+    "6" "Italian  (Italiano)        it_IT" \
+    3>&1 1>&2 2>&3 )
+
 
     case ${LANG_OPTION} in
         1)
