4210419bde5dfb42ddc52c5af4ecbdbf28776033
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-28 20:22:15 +0000

    update aerporto Pt

--- functions/modulemetar_setup_pt.sh
+++ functions/modulemetar_setup_pt.sh
@@ -7,51 +7,53 @@ whiptail --title "Metar Info" --yesno "Queres configurar este modulo?" 8 78  3>&
     then
         sleep 1
     selected=$(whiptail --title "Metar Info" --checklist "Escolhe os Aeroportos: Podes alterar tambem no Painel" 24 78 17 \
-        "EGLL" "London Heathrow" OFF \
-        "EGKK" "London Gatwick" OFF \
-        "EGCC" "Manchester" OFF \
-        "EGBB" "Birmingham" OFF \
-        "EGSS" "London Stansted" OFF \
-        "EGPF" "Glasgow" OFF \
-        "EGPH" "Edinburgh" OFF \
-        "EGPD" "Aberdeen Dyce" OFF \
-        "EGPK" "Prestwick" OFF \
-        "EGHH" "Bournemouth" OFF \
-        "EGHI" "Southampton" OFF \
-        "EGNT" "Newcastle" OFF \
-        "EGNX" "East Midlands" OFF \
-        "EGGW" "London Luton" OFF \
-        "EGGD" "Bristol" OFF \
-        "EGCN" "Sheffield" OFF \
-        "EGNM" "Leeds Bradford" OFF \
-        "EGNS" "Ronaldsway" OFF \
-        "EGAA" "Belfast Aldergrove" OFF \
-        "EGBD" "Belfast City" OFF 3>&1 1>&2 2>&3)
+        "LPCO" "Coimbra" OFF \
+        "LPCS" "Cascais" OFF \
+        "LPFL" "Flores" OFF \
+        "LPGR" "Graciosa" OFF \
+        "LPHR" "Horta" OFF \
+        "LPPE" "Évora" OFF \
+        "LPPI" "Pico" OFF \
+        "LPPS" "Porto Santo" OFF \
+        "LPPT" "Portimã" OFF \
+        "LPSJ" "São Jorge" OFF \
+        "LPST" "Sintra" OFF \
+        "LPVZ" "Gonçalves Lobato" OFF \
+        "LPVR" "Vila Real" OFF \
+        "LPMA" "Madeira" OFF \
+        "LPPT" "Lisboa" OFF \
+        "LPPR" "Porto" OFF \
+        "LPFR" "Faro" OFF \
+        "LPBG" "Braga" OFF \
+        "LPBJ" "Beja" OFF \
+        "LPBR" "Bragança" OFF \
+        "LPFR" "Funchal" OFF 3>&1 1>&2 2>&3)
         selected=$(echo "$selected" | sed 's/"//g')
         selected=$(echo "$selected" | tr ' ' ',')
         sed -i "s/AIRPORTS=.*/AIRPORTS=$selected/g"  /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
         sleep 5
     specific_airport=$(whiptail --title "Metar Info" --radiolist "Escolhe o codigo ICAO para o Aeroporto por defeito: " 24 78 17 \
-        "EGLL" "London Heathrow" OFF \
-        "EGKK" "London Gatwick" OFF \
-        "EGCC" "Manchester" OFF \
-        "EGBB" "Birmingham" OFF \
-        "EGSS" "London Stansted" OFF \
-        "EGPF" "Glasgow" OFF \
-        "EGPH" "Edinburgh" OFF \
-        "EGPD" "Aberdeen Dyce" OFF \
-        "EGPK" "Prestwick" OFF \
-        "EGHH" "Bournemouth" OFF \
-        "EGHI" "Southampton" OFF \
-        "EGNT" "Newcastle" OFF \
-        "EGNX" "East Midlands" OFF \
-        "EGGW" "London Luton" OFF \
-        "EGGD" "Bristol" OFF \
-        "EGCN" "Sheffield" OFF \
-        "EGNM" "Leeds Bradford" OFF \
-        "EGNS" "Ronaldsway" OFF \
-        "EGAA" "Belfast Aldergrove" OFF \
-        "EGBD" "Belfast City" OFF 3>&1 1>&2 2>&3)
+        "LPCO" "Coimbra" OFF \
+        "LPCS" "Cascais" OFF \
+        "LPFL" "Flores" OFF \
+        "LPGR" "Graciosa" OFF \
+        "LPHR" "Horta" OFF \
+        "LPPE" "Évora" OFF \
+        "LPPI" "Pico" OFF \
+        "LPPS" "Porto Santo" OFF \
+        "LPPT" "Portimã" OFF \
+        "LPSJ" "São Jorge" OFF \
+        "LPST" "Sintra" OFF \
+        "LPVZ" "Gonçalves Lobato" OFF \
+        "LPVR" "Vila Real" OFF \
+        "LPMA" "Madeira" OFF \
+        "LPPT" "Lisboa" OFF \
+        "LPPR" "Porto" OFF \
+        "LPFR" "Faro" OFF \
+        "LPBG" "Braga" OFF \
+        "LPBJ" "Beja" OFF \
+        "LPBR" "Bragança" OFF \
+        "LPFR" "Funchal" OFF 3>&1 1>&2 2>&3)
         specific_airport=$(echo "$specific_airport" | sed 's/"//g')
         sed -i "s/\STARTDEFAULT=.*/STARTDEFAULT=$specific_airport/g" /etc/svxlink/svxlink.d/ModuleMetarInfo.conf
         echo -e "$(date)" "${GREEN} $selected Aeroportos incluidos no aeroporto por defeito $specific_airport ${NORMAL}" | sudo tee -a /var/log/install.log   
