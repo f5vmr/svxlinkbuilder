b37c0e1f3498369ba608ad726c2fc7716eac9537
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-28 20:54:25 +0000

    update es sa818

--- functions/sa818_test_es.sh
+++ functions/sa818_test_es.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function sa818_test_es {
-if (whiptail --title "SA818 Device Check" --defaultno --yesno "Do you have an SA818 device fitted as a transceiver in your hotspot? If you do not then select 'No' otherwise select 'Yes'." 10 78); then
+if (whiptail --title "SA818 Device Check" --defaultno --yesno "¿Tiene un dispositivo SA818 instalado como transceptor en su punto de acceso? Si no, seleccione "No"; de lo contrario, seleccione "Sí"." 10 78); then
     sa818=true
 else
     sa818=false
