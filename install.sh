bc6afbfd68b2b054dc0e23860d42bdbed10b5fae
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-28 12:33:24 +0000

    update - Language selection

--- install.sh
+++ install.sh
@@ -1,42 +1,39 @@
+#!/bin/bash
 #### INSTALLATION SCRIPT ####
 # Setting non-superuser elements #
 #### INITIALISE ####
-# clear
+
 source "${BASH_SOURCE%/*}/functions/initialise.sh"
 initialise
+
 cd /home/pi
-echo -e "${GREEN} #### OS = $operating_system and Current user = $logname #### ${NORMAL}" | sudo tee -a  /var/log/install.log
+
+# Log OS and user (screen + log)
+echo -e "${GREEN} #### OS = $operating_system and Current user = $logname #### ${NORMAL}" | tee -a /var/log/install.log > /dev/null
+
 #### SuperUser Install ####
 #### LANGUAGE ####
 source "${BASH_SOURCE%/*}/functions/language.sh"
 which_language
 
-if  [[ "$LANG" == "fr_FR.UTF-8" ]] 
-then
-##French
-sudo ./svxlinkbuilder/install_main_fr.sh
-
-elif  [[ "$LANG" == "en_US.UTF-8" ]]
-##US English
-then
-sudo ./svxlinkbuilder/install_main.sh
-
-elif  [[ "$LANG" == "es_ES.UTF-8" ]] 
-then
-##Spanish
-sudo ./svxlinkbuilder/install_main_es.sh
-
-elif  [[ "$LANG" == "pt_PT.UTF-8" ]] 
-then
-##Portuguese
-sudo ./svxlinkbuilder/install_main_pt.sh
-
-elif  [[ "$LANG" == "it_IT.UTF-8" ]] 
-then
-##Italian
-sudo ./svxlinkbuilder/install_main_it.sh
-
-else
-##UK English
-sudo ./svxlinkbuilder/install_main.sh
-fi
+# Run language-specific installer
+case "$LANG" in
+    fr_FR.UTF-8)
+        sudo ./svxlinkbuilder/install_main_fr.sh
+        ;;
+    en_US.UTF-8)
+        sudo ./svxlinkbuilder/install_main.sh
+        ;;
+    es_ES.UTF-8)
+        sudo ./svxlinkbuilder/install_main_es.sh
+        ;;
+    pt_PT.UTF-8)
+        sudo ./svxlinkbuilder/install_main_pt.sh
+        ;;
+    it_IT.UTF-8)
+        sudo ./svxlinkbuilder/install_main_it.sh
+        ;;
+    *)
+        sudo ./svxlinkbuilder/install_main.sh  # Default UK English
+        ;;
+esac
