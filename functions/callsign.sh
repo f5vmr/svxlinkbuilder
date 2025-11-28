03903d43217483b93a0d9f5b7646028e22256e86
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-28 15:26:42 +0000

    update

--- functions/callsign.sh
+++ functions/callsign.sh
@@ -2,8 +2,11 @@
 function callsign {
 get_CallVar() {
     call=$(whiptail --inputbox "Enter the node callsign:" 8 78 3>&1 1>&2 2>&3)
-    echo -e "${CYAN}$call${WHITE}" | sudo tee -a /var/log/install.log > /dev/null 
+    call="${call^^}"  # Uppercase
+    echo -e "${CYAN}$call${WHITE}" | sudo tee -a /var/log/install.log > /dev/null
+    echo "$call"
 }
+
 
 while true; do
     user_input=$(get_CallVar)
