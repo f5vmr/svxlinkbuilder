8a34c770de0cfe45c503ff0a590ceda09d831403
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-30 18:56:07 +0000

    update w/o service

--- functions/initialise.sh
+++ functions/initialise.sh
@@ -20,49 +20,7 @@ WHITE='\033[0;37m' # normal
 NORMAL='\033[0m' # normal
 locale=""
 lang=""
-## Replace default svxlink.service everytime with our modified version
-echo -e "$(date)" "${BLUE} #### Replacing svxlink.service with modified version #### ${NORMAL}" | sudo tee -a /var/log/install.log
-SERVICE_FILE="/usr/lib/systemd/system/svxlink.service"
-if [ -f "$SERVICE_FILE" ]; then
-    sudo rm -f "$SERVICE_FILE"
-fi
-sudo tee "$SERVICE_FILE" > /dev/null <<EOL
-[Unit]
-Description=SvxLink repeater control software (Universal)
-After=network.target sound.target systemd-udev-settle.service
-Wants=systemd-udev-settle.service
-Documentation=man:svxlink(1) man:svxlink.conf(5)
 
-[Service]
-Type=simple
-
-# Load admin overrides if present
-EnvironmentFile=-/etc/default/svxlink
-
-# Hard defaults — used if the file above is missing or empty
-Environment=RUNASUSER=svxlink
-Environment=CFGFILE=/etc/svxlink/svxlink.conf
-Environment=LOGFILE=/var/log/svxlink.log
-Environment=STATEDIR=/var/lib/svxlink
-
-UMask=0002
-WorkingDirectory=/etc/svxlink
-
-# ---- Main program ----
-ExecStart=/usr/bin/svxlink --logfile=${LOGFILE} --config=${CFGFILE} --runasuser=${RUNASUSER}
-
-ExecStopPost=/usr/bin/svxlink --config=${CFGFILE} --runasuser=${RUNASUSER} --reset
-ExecReload=/bin/kill -s HUP $MAINPID
-
-Restart=on-failure
-Nice=-10
-TimeoutStartSec=60
-TimeoutStopSec=10
-LimitCORE=infinity
-
-[Install]
-WantedBy=multi-user.target
-EOL
 ## Ensure svxlink.log exists and has correct ownership
 sudo touch /var/log/svxlink.log
 sudo chown svxlink:svxlink /var/log/svxlink.log
